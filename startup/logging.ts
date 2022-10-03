61d88d7503d6338cb39c3cff67ef67bdd8e3bca2
iskewedi <jtornello2019@hotmail.com>
2022-10-03 09:45:57 -0300

    - Improved logging relative route

--- startup/logging.ts
+++ startup/logging.ts
@@ -7,7 +7,7 @@ import 'express-async-errors'; //Envolves router async functions in try/catch bl
 module.exports = function () {
   //Uncaught exception configs
   winston.exceptions.handle(
-    new winston.transports.File({ filename: '../uncaughtExceptions.log' }),
+    new winston.transports.File({ filename: './uncaughtExceptions.log' }),
     new winston.transports.Console({ format: winston.format.simple() })
   );
   //Trick to handle Promise Rejections too with winston
@@ -16,7 +16,7 @@ module.exports = function () {
   });
 
   //Logger configuration
-  winston.add(new winston.transports.File({ filename: '../logfile.log' }));
+  winston.add(new winston.transports.File({ filename: './logfile.log' }));
   winston.add(
     new winston.transports.Console({
       format: winston.format.simple(),
